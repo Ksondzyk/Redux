32030a34859c5f79536cb7bdc4cf02c4f9e9d5d9
roman.kson@gmail.com <roman.kson@gmail.com>
2020-07-23 17:27:05 +0300

    push

--- lesson2/hw1/src/index.js
+++ lesson2/hw1/src/index.js
@@ -1,3 +1,14 @@
 import store from "./store";
+import { addUser, deleteUser } from "./users.actions";
 
-console.log(store.getState());
+console.log(store.dispatch(addUser));
+
+// const add = [
+//   { id: 77, name: "roma" },
+//   { id: 78, name: "Masha" },
+// ];
+
+// const res = add.concat({ id: 76, name: "Sarah" });
+// console.log(res);
+// const result = res.filter(({ id }) => id !== 76);
+// console.log(result);
