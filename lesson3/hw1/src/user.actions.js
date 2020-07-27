f41a3c64f2e5db12a3b0f382d105ce8d3c98e3de
roman.kson@gmail.com <roman.kson@gmail.com>
2020-07-27 19:46:07 +0300

    push

--- lesson3/hw1/src/user.actions.js
+++ lesson3/hw1/src/user.actions.js
@@ -1,7 +1,7 @@
 export const SET_USER = "USERS/SET_USER";
 export const REMOVE_USER = "USERS/REMOVE_USER ";
 
-export const setUser = (userData = null) => {
+export const setUser = (userData) => {
   return {
     type: SET_USER,
     payload: {
--- lesson3/hw1/src/user.reducer.js
+++ lesson3/hw1/src/user.reducer.js
@@ -1,6 +1,6 @@
 import { SET_USER, REMOVE_USER } from "./user.actions";
 
-export const usersReducer = (state = "", action) => {
+export const usersReducer = (state = null, action) => {
   switch (action.type) {
     case SET_USER: {
       return action.payload.userData;
