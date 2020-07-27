ad69604d4d68ac68fd61e18307f0d607a5145c20
roman.kson@gmail.com <roman.kson@gmail.com>
2020-07-27 15:26:09 +0300

    react dev tools

--- lesson3/task1/src/store.js
+++ lesson3/task1/src/store.js
@@ -7,6 +7,9 @@ const appReducer = combineReducers({
   counter: conterReducer,
   users: usersReducer,
 });
-const store = createStore(appReducer);
+const store = createStore(
+  appReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
