d6e7b44d68181ad2d3f103b631ff18b372578293
roman.kson@gmail.com <roman.kson@gmail.com>
2020-07-27 19:33:53 +0300

    redux

--- lesson3/hw1/src/index.js
+++ lesson3/hw1/src/index.js
@@ -6,14 +6,5 @@ import { setUser, removeUser } from "./users.actions";
 
 store.subscribe(() => console.log(store.getState()));
 store.dispatch(setUser("roman"));
-console.log(store.dispatch(setLanguage()));
-console.log(store.dispatch(addProduct({ id: 1, name: "milk" })));
-
-// store.dispatch(
-//   addUser({
-//     id: 2,
-//     name: "Bob",
-//   })
-// );
-// store.dispatch(deleteUser(2));
-// store.dispatch(updateUser(1, { name: "Bob Martin", age: 25 }));
+store.dispatch(setLanguage());
+store.dispatch(addProduct({ id: 1, name: "milk" }));
