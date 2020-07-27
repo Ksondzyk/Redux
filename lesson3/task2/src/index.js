caa80f114278f9380eaf115e4a4975abcfa3bd17
roman.kson@gmail.com <roman.kson@gmail.com>
2020-07-27 15:30:52 +0300

    redux dev tools

--- lesson3/task2/src/index.js
+++ lesson3/task2/src/index.js
@@ -1,29 +1,10 @@
-import "./index.scss";
-import store, { increment, decrement, reset } from "./store";
+import { store, increment, decrement } from "./counter";
 
-const resultElem = document.querySelector(".counter__result");
-const incrementBtn = document.querySelector('[data-action="increment"]');
-const resetBtn = document.querySelector('[data-action="reset"]');
-const decrementBtn = document.querySelector("[data-action='decrement']");
+console.log(store.getState());
 
-const onIcrement = () => {
-  store.dispatch(increment());
-};
-const onDecrement = () => {
-  store.dispatch(decrement());
-};
-const onReset = () => {
-  store.dispatch(reset());
-};
-incrementBtn.addEventListener("click", onIcrement);
-decrementBtn.addEventListener("click", onDecrement);
-resetBtn.addEventListener("click", onReset);
+store.subscribe(() => console.log(store.getState()));
 
-store.subscribe(() => {
-  console.log(store.getState());
-  const state = store.getState();
-  const currentValue = state.history.reduce((acc, value) => acc + +value, 0);
-  const historyString = state.history.join("");
-  resultElem.textContent =
-    state.history.length === 0 ? "" : `${historyString} = ${currentValue}`;
-});
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
