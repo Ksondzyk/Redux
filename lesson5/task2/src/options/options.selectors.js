316b81193bb9ebcf03cd8f2868c78c1d2edf46da
roman.kson@gmail.com <roman.kson@gmail.com>
2020-07-29 16:38:14 +0300

    react Redux selectors

---
+++ lesson5/task2/src/options/options.selectors.js
@@ -0,0 +1,32 @@
+import { createSelector } from "reselect";
+
+export const optionsListSelector = (state) => {
+  return state.options.optionsList;
+};
+export const selectedIdSelector = (state) => {
+  return state.options.selected;
+};
+export const selectedOptionSelector = createSelector(
+  [optionsListSelector, selectedIdSelector],
+  (allOptionsList, selectedIds) => {
+    return allOptionsList.filter(({ id }) => selectedIds.includes(id));
+  }
+);
+
+// export const selectedOptionSelector = (state) => {
+//   const allOptionsList = optionsListSelector(state);
+//   const selectedIds = selectedIdSelector(state);
+//   return allOptionsList.filter(({ id }) => selectedIds.includes(id));
+// };
+export const avialableOptionSelector = createSelector(
+  [optionsListSelector, selectedIdSelector],
+  (allOptionsList, selectedIds) => {
+    return allOptionsList.filter(({ id }) => !selectedIds.includes(id));
+  }
+);
+
+// export const avialableOptionSelector = (state) => {
+//   const allOptionsList = optionsListSelector(state);
+//   const selectedIds = selectedIdSelector(state);
+//   return allOptionsList.filter(({ id }) => !selectedIds.includes(id));
+// };
