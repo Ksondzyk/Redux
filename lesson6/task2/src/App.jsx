4d2cff5fcacbe8abda4e598506a7521301166d2e
roman.kson@gmail.com <roman.kson@gmail.com>
2020-08-04 13:42:49 +0300

    push

--- lesson6/task2/src/App.jsx
+++ lesson6/task2/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import store from "./store";
-import UserInfo from "./users/UserInfo ";
+import UserInfo from "./users/UserInfo";
 import SearchField from "./users/SearchField";
 
 const App = () => {
