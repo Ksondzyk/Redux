a6c2803d33958e3f5051ef796c10eaa8624cb8b1
roman.kson@gmail.com <roman.kson@gmail.com>
2020-08-05 22:38:42 +0300

    push

--- lesson6/hw1/src/weather/Weather.jsx
+++ lesson6/hw1/src/weather/Weather.jsx
@@ -1,27 +1,31 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
+
 import { fetchUserdata } from "./weather.actions";
 import { userCitiesSelector } from "./users.selectors";
 
-const Weather = (props) => {
-  console.log(props);
+const Weather = ({ fetchUserdata, userData }) => {
   useEffect(() => {
-    props.fetchUserdata();
-  }, [props, props.userData]);
-
-  if (props.userData === null) {
-    return null;
-  }
+    fetchUserdata();
+  }, [userData]);
   return (
     <main className="weather">
       <h1 className="weather__title">Weather data</h1>
       <ul className="cities-list">
-        {props.userData(({ id, temperature, name }) => (
+        {userData(({ id, temperature, name }) => (
           <li className="city" key={id}>
             <span className="city__name">{name}</span>
             <span className="city__temperature">{temperature}</span>
           </li>
         ))}
+        {/* <li className="city">
+          <span className="city__name">name</span>
+          <span className="city__temperature">temperature</span>
+        </li>
+        <li className="city">
+          <span className="city__name">name</span>
+          <span className="city__temperature">temperature</span>
+        </li> */}
       </ul>
     </main>
   );
